fbb7a505daa871db2b79ea4ecbc6a51324707ed6
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-11-23 17:20:12 -0300

    Geração do Arquivo .py gerenciamento de dependencias com Poetry

--- mvp_ml.py
+++ mvp_ml.py
@@ -1,24 +1,39 @@
----
-title: MVP Machine Learning
-jupyter:
-  jupytext:
-    text_representation:
-      extension: .qmd
-      format_name: quarto
-      format_version: '1.0'
-      jupytext_version: 1.16.4
-  kernelspec:
-    display_name: ml
-    language: python
-    name: python3
-format:
-  html:
-    embed-resources: true
----
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: ml
+#     language: python
+#     name: python3
+# ---
 
-<a target="_blank" href="https://colab.research.google.com/github/vinicius-souza-lima/mvp_ml/blob/main/mvp_ml.ipynb"> <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/> </a>
+# %% [markdown]
+# ---
+# title: MVP Machine Learning
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .qmd
+#       format_name: quarto
+#       format_version: '1.0'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: ml
+#     language: python
+#     name: python3
+# format:
+#   html:
+#     embed-resources: true
+# ---
+#
+# <a target="_blank" href="https://colab.research.google.com/github/vinicius-souza-lima/mvp_ml/blob/main/mvp_ml.ipynb"> <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/> </a>
 
-```{python}
+# %%
 from pathlib import Path
 import urllib.request
 import pandas as pd
@@ -29,11 +44,12 @@ from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix,precision_score,recall_score,f1_score
 import joblib
-```
 
-## Problema de Regressão
 
-```{python}
+# %% [markdown]
+# ## Problema de Regressão
+
+# %%
 def load_data(file : str,url : str,**kwargs):
     """
     Função para carregar os dados, verifica se dataset já foi baixado, baixando-o de acordo
@@ -46,23 +62,16 @@ def load_data(file : str,url : str,**kwargs):
             raise ValueError("Url não informada")
         urllib.request.urlretrieve(url,file_path)
     return pd.read_csv(file,**kwargs)
-```
 
-```{python}
-import numpy as np
-import matplotlib.pyplot as plt
-a = np.linspace(1.,1000.0,num=100)
-plt.plot(a)
-plt.show()
-```
 
-## Problema de Classificação (Algoritmos Clássicos)
+# %% [markdown]
+# ## Problema de Classificação (Algoritmos Clássicos)
+#
+# ### Criação do Dataset simplificado a partir das imagens
+#
+# Objetivo: Classificar imagem de raio x de paciente em saudável, pneumonia bacteriana ou pneumonia virótica
 
-### Criação do Dataset simplificado a partir das imagens
-
-Objetivo: Classificar imagem de raio x de paciente em saudável, pneumonia bacteriana ou pneumonia virótica
-
-```{python}
+# %%
 def convert_toarray(path_dir:str,target_values:list[str],fallback:str|None=None,
                 resolution: tuple[int,int]=(128,128)):
     dir = Path(path_dir)
@@ -78,96 +87,81 @@ def convert_toarray(path_dir:str,target_values:list[str],fallback:str|None=None,
                              if cat in target_values),fallback)) # procura categoria no nome do arquivo
 
     return np.stack(imgs),np.array(targets)
-```
 
-```{python}
+
+# %%
 resolution = (128,128)
 #X_chest,y_chest = convert_toarray("./datasets/chest_xray/imgs",["virus","bacteria"],"normal",resolution)
-```
 
-```{python}
+# %%
 #for i,array in enumerate(np.array_split(X_chest,5)):
     #np.save(f"datasets/chest_xray/imgs_array/X_chest_{i}",array)
 #np.save("datasets/chest_xray/imgs_array/y_chest",y_chest)
-```
 
-```{python}
+# %%
 X_chest = []
 for file in sorted(Path("datasets/chest_xray/imgs_array/X_chest/").glob("*")):
     X_chest.append(np.load(str(file)))
 X_chest = np.vstack(X_chest)
 y_chest = np.load("datasets/chest_xray/imgs_array/y_chest.npy")
-```
 
-```{python}
+# %%
 X_chest.shape
-```
 
-```{python}
+# %%
 plt.imshow(X_chest[4000].reshape(resolution))
 plt.axis("off")
 plt.show()
-```
 
-```{python}
+# %%
 X_chest_train,X_chest_test,y_chest_train,y_chest_test =train_test_split(X_chest,y_chest,test_size=0.2,
                                   random_state=42,
                                   stratify=y_chest)
-```
 
-```{python}
+# %%
 #lr_clf = LogisticRegression(max_iter=1000,random_state=42)
 #lr_clf.fit(X_chest_train,y_chest_train)
-```
 
-```{python}
+# %%
 #joblib.dump(lr_clf,"Logistic_Regression.joblib")
-```
 
-```{python}
+# %%
 lr_clf = joblib.load("Logistic_Regression.joblib")
-```
 
-```{python}
+# %%
 #y_chest_pred = cross_val_predict(lr_clf,X_chest_train,y_chest_train,cv=3)
 #np.save("y_chest_pred",y_chest_pred)
-```
 
-```{python}
+# %%
 y_chest_pred = np.load("y_chest_pred.npy")
-```
 
-```{python}
+# %%
 cm_chest = confusion_matrix(y_chest_train,y_chest_pred)
-```
 
-```{python}
+# %%
 cm_chest
-```
 
-```{python}
+# %%
 display(precision_score(y_chest_train,y_chest_pred,average=None))
 display(precision_score(y_chest_train,y_chest_pred,average="macro"))
 display(precision_score(y_chest_train,y_chest_pred,average="micro"))
 display(precision_score(y_chest_train,y_chest_pred,average="weighted"))
-```
 
-```{python}
+# %%
 display(recall_score(y_chest_train,y_chest_pred,average=None))
 display(recall_score(y_chest_train,y_chest_pred,average="macro"))
 display(recall_score(y_chest_train,y_chest_pred,average="micro"))
 display(recall_score(y_chest_train,y_chest_pred,average="weighted"))
-```
 
-```{python}
+# %%
 display(f1_score(y_chest_train,y_chest_pred,average=None))
 display(f1_score(y_chest_train,y_chest_pred,average="macro"))
 display(f1_score(y_chest_train,y_chest_pred,average="micro"))
 display(f1_score(y_chest_train,y_chest_pred,average="weighted"))
-```
 
-## Problema de Visão Computacional (Deep Learning)
-
-Objetivo: Treinar rede neural que classifique os tipos de tumores e segmente na imagem o local em que ele aparece
-
-## Problema de Processamento de Linguagem Natural
+# %% [markdown]
+# ## Problema de Visão Computacional (Deep Learning)
+#
+# Objetivo: Treinar rede neural que classifique os tipos de tumores e segmente na imagem o local em que ele aparece
+#
+# ## Problema de Processamento de Linguagem Natural
