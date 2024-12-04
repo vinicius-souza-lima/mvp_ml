79d0a41f6661e9bbf57c8467238472d98621e76a
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-12-04 18:39:27 -0300

    Inclusão da classe Dataset

--- mvp_ml.py
+++ mvp_ml.py
@@ -7,30 +7,12 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.16.4
 #   kernelspec:
-#     display_name: ml
+#     display_name: .venv
 #     language: python
 #     name: python3
 # ---
 
 # %% [markdown]
-# ---
-# title: MVP Machine Learning
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .qmd
-#       format_name: quarto
-#       format_version: '1.0'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: ml
-#     language: python
-#     name: python3
-# format:
-#   html:
-#     embed-resources: true
-# ---
-#
 # <a target="_blank" href="https://colab.research.google.com/github/vinicius-souza-lima/mvp_ml/blob/main/mvp_ml.ipynb"> <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/> </a>
 
 # %%
@@ -42,7 +24,7 @@ import numpy as np
 from PIL import Image
 from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix,precision_score,recall_score,f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 import joblib
 
 
@@ -50,19 +32,6 @@ import joblib
 # ## Problema de Regressão
 
 # %%
-def load_data(file : str,url : str,**kwargs):
-    """
-    Função para carregar os dados, verifica se dataset já foi baixado, baixando-o de acordo
-    com a necessidade
-    """
-    file_path = Path(file)
-    if not file_path.is_file():
-        Path("datasets").mkdir(parents=True, exist_ok=True)
-        if url is None:
-            raise ValueError("Url não informada")
-        urllib.request.urlretrieve(url,file_path)
-    return pd.read_csv(file,**kwargs)
-
 
 # %% [markdown]
 # ## Problema de Classificação (Algoritmos Clássicos)
@@ -72,31 +41,66 @@ def load_data(file : str,url : str,**kwargs):
 # Objetivo: Classificar imagem de raio x de paciente em saudável, pneumonia bacteriana ou pneumonia virótica
 
 # %%
-def convert_toarray(path_dir:str,target_values:list[str],fallback:str|None=None,
-                resolution: tuple[int,int]=(128,128)):
-    dir = Path(path_dir)
-    files = dir.glob("*")
-    imgs = []
-    targets = []
-    for f in files:
-        imgs.append(np.array(Image.open(str(f)) # Lê a imagem
-                                  .convert("L")  # Converte para escala de cinza
-                                  .resize(resolution,Image.Resampling.LANCZOS)) # Redimensiona a imagem
-                                  .flatten()) # Formata a matriz como array 
-        targets.append(next((cat for cat in f.name.split("_")
-                             if cat in target_values),fallback)) # procura categoria no nome do arquivo
+class Dataset:
+    def __init__(
+        self,
+        path_dir: str,
+        file: str,
+        url: str,
+        resolution: tuple[int, int] = (128, 128),
+    ):
+        self.data = None
+        self.path_dir = path_dir
+        self.url = url
+        self.resolution = resolution
 
-    return np.stack(imgs),np.array(targets)
+    def load_csv(self, **kwargs):
+        """
+        Função para carregar os dados, verifica se dataset já foi baixado, baixando-o de acordo
+        com a necessidade
+        """
+        file_path = Path(self.path_dir)
+        if not file_path.is_file():
+            Path("datasets").mkdir(parents=True, exist_ok=True)
+            if self.url is None:
+                raise ValueError("Url não informada")
+            urllib.request.urlretrieve(self.url, file_path)
+        self.data = pd.read_csv(self.path_dir, **kwargs)
+
+    def convert_toarray(
+        self,
+        target_values: list[str],
+        fallback: str | None = None,
+    ):
+        dir = Path(self.path_dir)
+        files = dir.glob("*")
+        imgs = []
+        targets = []
+        for f in files:
+            imgs.append(
+                np.array(
+                    Image.open(str(f))  # Lê a imagem
+                    .convert("L")  # Converte para escala de cinza
+                    .resize(self.resolution, Image.Resampling.LANCZOS)
+                ).flatten()  # Redimensiona a imagem
+            )  # Formata a matriz como array
+            targets.append(
+                next(
+                    (cat for cat in f.name.split("_") if cat in target_values), fallback
+                )
+            )  # procura categoria no nome do arquivo
+
+        return np.stack(imgs), np.array(targets)
 
 
 # %%
-resolution = (128,128)
-#X_chest,y_chest = convert_toarray("./datasets/chest_xray/imgs",["virus","bacteria"],"normal",resolution)
+resolution = (128, 128)
+# X_chest,y_chest = convert_toarray("./datasets/chest_xray/imgs",["virus","bacteria"],"normal",resolution)
 
 # %%
-#for i,array in enumerate(np.array_split(X_chest,5)):
-    #np.save(f"datasets/chest_xray/imgs_array/X_chest_{i}",array)
-#np.save("datasets/chest_xray/imgs_array/y_chest",y_chest)
+# for i,array in enumerate(np.array_split(X_chest,5)):
+# np.save(f"datasets/chest_xray/imgs_array/X_chest_{i}",array)
+# np.save("datasets/chest_xray/imgs_array/y_chest",y_chest)
 
 # %%
 X_chest = []
@@ -114,50 +118,53 @@ plt.axis("off")
 plt.show()
 
 # %%
-X_chest_train,X_chest_test,y_chest_train,y_chest_test =train_test_split(X_chest,y_chest,test_size=0.2,
-                                  random_state=42,
-                                  stratify=y_chest)
+X_chest_train, X_chest_test, y_chest_train, y_chest_test = train_test_split(
+    X_chest, y_chest, test_size=0.2, random_state=42, stratify=y_chest
+)
 
 # %%
-#lr_clf = LogisticRegression(max_iter=1000,random_state=42)
-#lr_clf.fit(X_chest_train,y_chest_train)
+# lr_clf = LogisticRegression(max_iter=1000,random_state=42)
+# lr_clf.fit(X_chest_train,y_chest_train)
 
 # %%
-#joblib.dump(lr_clf,"Logistic_Regression.joblib")
+# joblib.dump(lr_clf,"Logistic_Regression.joblib")
 
 # %%
 lr_clf = joblib.load("Logistic_Regression.joblib")
 
 # %%
-#y_chest_pred = cross_val_predict(lr_clf,X_chest_train,y_chest_train,cv=3)
-#np.save("y_chest_pred",y_chest_pred)
+# y_chest_pred = cross_val_predict(lr_clf,X_chest_train,y_chest_train,cv=3)
+# np.save("y_chest_pred",y_chest_pred)
+
+# %% [markdown]
+# # Selecionar modelo
 
 # %%
 y_chest_pred = np.load("y_chest_pred.npy")
 
 # %%
-cm_chest = confusion_matrix(y_chest_train,y_chest_pred)
+cm_chest = confusion_matrix(y_chest_train, y_chest_pred)
 
 # %%
 cm_chest
 
 # %%
-display(precision_score(y_chest_train,y_chest_pred,average=None))
-display(precision_score(y_chest_train,y_chest_pred,average="macro"))
-display(precision_score(y_chest_train,y_chest_pred,average="micro"))
-display(precision_score(y_chest_train,y_chest_pred,average="weighted"))
+display(precision_score(y_chest_train, y_chest_pred, average=None))
+display(precision_score(y_chest_train, y_chest_pred, average="macro"))
+display(precision_score(y_chest_train, y_chest_pred, average="micro"))
+display(precision_score(y_chest_train, y_chest_pred, average="weighted"))
 
 # %%
-display(recall_score(y_chest_train,y_chest_pred,average=None))
-display(recall_score(y_chest_train,y_chest_pred,average="macro"))
-display(recall_score(y_chest_train,y_chest_pred,average="micro"))
-display(recall_score(y_chest_train,y_chest_pred,average="weighted"))
+display(recall_score(y_chest_train, y_chest_pred, average=None))
+display(recall_score(y_chest_train, y_chest_pred, average="macro"))
+display(recall_score(y_chest_train, y_chest_pred, average="micro"))
+display(recall_score(y_chest_train, y_chest_pred, average="weighted"))
 
 # %%
-display(f1_score(y_chest_train,y_chest_pred,average=None))
-display(f1_score(y_chest_train,y_chest_pred,average="macro"))
-display(f1_score(y_chest_train,y_chest_pred,average="micro"))
-display(f1_score(y_chest_train,y_chest_pred,average="weighted"))
+display(f1_score(y_chest_train, y_chest_pred, average=None))
+display(f1_score(y_chest_train, y_chest_pred, average="macro"))
+display(f1_score(y_chest_train, y_chest_pred, average="micro"))
+display(f1_score(y_chest_train, y_chest_pred, average="weighted"))
 
 # %% [markdown]
 # ## Problema de Visão Computacional (Deep Learning)
